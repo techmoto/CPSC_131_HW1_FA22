2250450d2ce4d31eaf073f94bf5760622bd9f292
techmoto <103876700+techmoto@users.noreply.github.com>
2022-09-09 20:24:36 -0700

    Update vector131.h

--- vector131.h
+++ vector131.h
@@ -1,1 +1,36 @@
+#pragma once
 
+class vector131 {
+	private:
+		int *arrayPointer;
+		int numOfElements; 
+		int nextIndex; 
+	public:
+		vector131();  // constructor
+		~vector131(); // destructor
+		void push_front(int val); // adds a new value to the front
+		void push_back(int val); // adds a new value to the end
+		int resize(); // resizes array
+};
+
+//constructor
+vector131::vector131() {
+
+}
+
+//destructor
+vector131::~vector131() {
+
+}
+
+void vector131::push_front(int val) {
+
+}
+
+void vector131::push_back(int val) {
+
+}
+
+int vector131::resize() {
+
+}
