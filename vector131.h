0c30f73bdee3c8a84ccc90a84d9724cac309846d
techmoto <103876700+techmoto@users.noreply.github.com>
2022-09-09 20:45:23 -0700

    Update vector131.h

--- vector131.h
+++ vector131.h
@@ -8,8 +8,10 @@ class vector131 {
 	public:
 		vector131();  // constructor
 		~vector131(); // destructor
+		int& operator[](int index);
 		void push_front(int val); // adds a new value to the front
 		void push_back(int val); // adds a new value to the end
+		int size(); // returns array size
 		int resize(); // resizes array
 };
 
@@ -23,14 +25,25 @@ vector131::~vector131() {
 
 }
 
+// indexing operation
+int& vector131::operator[](int index) {
+
+}
+
+//push_front function
 void vector131::push_front(int val) {
 
 }
 
+//push_back function
 void vector131::push_back(int val) {
 
 }
 
-int vector131::resize() {
+//resizes the array
+int vector131::size() {
 
 }
+
+
+
