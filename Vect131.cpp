fed5f39392954e6168248455ae120fc79693e933
techmoto <103876700+techmoto@users.noreply.github.com>
2022-09-09 20:44:02 -0700

    Update Vect131.cpp

--- Vect131.cpp
+++ Vect131.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "Header.h"
 
+int main()
+{
+	vector131 newVector;
+	newVector.push_front(7);
+	newVector.push_back(8);
+	newVector.push_front(9);
+
+	for (int i = 0; i < newVector.size(); i++) {
+		std::cout << newVector[i] << std::endl;
+	}
+
+	return 0;
+}
